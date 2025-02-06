a58dffdff28a92ed0dbff180d94633a34c0cdeb0
nicoluvas <nicolaspoficial@gmail.com>
2025-02-06 19:17:26 -0300

    answers listing index and styles

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="post mb-6">
     <h1>
         <b>
             {{ $post->title }}
@@ -19,6 +20,21 @@
 
     <button onclick="location.href='{{ route('answers.create', $post) }}'"
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        answer
+        answer it
     </button>
+</div>
+
+    <div>
+        <h1 class="mb-4 text-4xl font-bold">answers</h1>
+        @forelse ($post->answers as $answer)
+            <div class="mb-4 answer border p-2">
+                <b> {{ $answer->title }}</b>
+                <p> {{ $answer->content }} </p>
+                <i>by {{ $answer->author->name }}</i>
+            </div>
+            <br>
+        @empty
+            no answers by now
+        @endforelse
+    </div>
 @endsection
